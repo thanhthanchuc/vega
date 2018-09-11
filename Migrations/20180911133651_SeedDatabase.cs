b3df1c59dca67ac0de3a689f3ecb50fa2898cf66
toiye <toiye@DESKTOP-BJB9GPG>
2018-09-11 20:46:01 +0700

    Seeding the Database

---
+++ Migrations/20180911133651_SeedDatabase.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace vega.Migrations
+{
+    public partial class SeedDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Makes (Name) values ('Make1')");
+            migrationBuilder.Sql("INSERT INTO Makes (Name) values ('Make2')");
+            migrationBuilder.Sql("INSERT INTO Makes (Name) values ('Make3')");
+
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeId) VALUES ('Make1-ModelA', 1)");
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeId) VALUES ('Make1-ModelB', 1)");
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeId) VALUES ('Make1-ModelC', 1)");
+
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeId) VALUES ('Make1-ModelA', 2)");
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeId) VALUES ('Make1-ModelB', 2)");
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeId) VALUES ('Make1-ModelC', 2)");
+
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeId) VALUES ('Make1-ModelA', 3)");
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeId) VALUES ('Make1-ModelB', 3)");
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeId) VALUES ('Make1-ModelC', 3)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            //Khi xóa Makes, tất cả các giá trị tham chiếu tới Makes đều bị xóa -- extension của EF.
+            migrationBuilder.Sql("DELETE FROM Makes");
+        }
+    }
+}
