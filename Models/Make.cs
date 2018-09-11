38ae03d19f7ce034bc49a57e0993bcdc5f3e054e
toiye <toiye@DESKTOP-BJB9GPG>
2018-09-11 20:35:37 +0700

    Applying Constraints and Name of Tables

--- Models/Make.cs
+++ Models/Make.cs
@@ -1,11 +1,17 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace vega.Models
 {
+    [Table("Makes")]
     public class Make
     {
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
         public ICollection<Model> Models { get; set; }
 
