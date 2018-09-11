9040e6a17e0f3aab106700d72320f53e98521bd1
toiye <toiye@DESKTOP-BJB9GPG>
2018-09-11 20:23:14 +0700

    First Migrations

---
+++ Migrations/20180911130706_InititalModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace vega.Migrations
+{
+    public partial class InititalModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- Persistance/VegaDbContext.cs
+++ Persistance/VegaDbContext.cs
@@ -1,9 +1,11 @@
 using Microsoft.EntityFrameworkCore;
+using vega.Models;
 
 namespace vega.Persistance
 {
     public class VegaDbContext : DbContext
     {
+        public DbSet<Make> Makes { get; set; }
         public VegaDbContext(DbContextOptions<VegaDbContext> obtions) : base(obtions)
         {
             
