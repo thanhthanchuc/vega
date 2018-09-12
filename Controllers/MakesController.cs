25606fb7d907d575d4db6ae30849dec88c99dde5
toiye <toiye@DESKTOP-BJB9GPG>
2018-09-12 10:04:33 +0700

    Building a API -/api/makes

---
+++ Controllers/MakesController.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using vega.Models;
+using vega.Persistance;
+
+namespace vega.Controllers
+{
+    public class MakesController : Controller
+    {
+        private readonly VegaDbContext _context;
+
+        public MakesController(VegaDbContext context)
+        {
+            _context = new VegaDbContext();
+        }
+
+        [HttpGet("/apt/makes")]
+        public async Task<IEnumerable<Make>> GetMakes()
+        {
+            return await _context.Makes.Include(c => c.Models).ToListAsync();
+        }
+    }
+}
--- Persistance/VegaDbContext.cs
+++ Persistance/VegaDbContext.cs
@@ -10,5 +10,9 @@ namespace vega.Persistance
         {
             
         }
+
+        public VegaDbContext()
+        {
+        }
     }
 }
