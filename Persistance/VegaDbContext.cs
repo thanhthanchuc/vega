b17027dcfcd5445d5f7417d6546aaa0482386161
toiye <toiye@DESKTOP-BJB9GPG>
2018-09-17 20:26:57 +0700

    Dealing with n-n relationship  and Update Database

--- Persistance/VegaDbContext.cs
+++ Persistance/VegaDbContext.cs
@@ -9,11 +9,17 @@ namespace vega.Persistance
         public DbSet<Make> Makes { get; set; }
         public VegaDbContext(DbContextOptions<VegaDbContext> obtions) : base(obtions)
         {
-            
+
         }
 
         public VegaDbContext()
         {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<VehicleFeature>().HasKey(vf =>
+                new { vf.FeatureId, vf.VehicleId });
         }
     }
 }
