5fd8bedbf1c5527519e2536d28cdeae6213aaec5
toiye <toiye@DESKTOP-BJB9GPG>
2018-09-17 09:10:41 +0700

    Create new Service

---
+++ ClientApp/src/services/make.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from "@angular/core";
+import { Http } from "@angular/http";
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class MakeService {
+
+    constructor(private http: Http) {
+    }
+
+    getMakes(){
+        return this.http.get('/api/makes')
+            .map(c => c.json());
+    }
+    
+}
