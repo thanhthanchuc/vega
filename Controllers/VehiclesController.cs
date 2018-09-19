d0f7f662105b6fb132065a12d98d5cb494312675
toiye <toiye@DESKTOP-BJB9GPG>
2018-09-19 10:10:59 +0700

    Mapping VehicleResource to Vehicle for HttpPost

--- Controllers/VehiclesController.cs
+++ Controllers/VehiclesController.cs
@@ -1,4 +1,6 @@
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using vega.Controllers.Resources;
 using vega.Models;
 
 namespace vega.Controllers
@@ -6,9 +8,15 @@ namespace vega.Controllers
     [Route("/api/vehicles")]
     public class VehiclesController : Controller
     {
-        [HttpPost]
-        public IActionResult CreateVehicle([FromBody] Vehicle vehicle)
+        private readonly IMapper mapper;
+        public VehiclesController(IMapper mapper)
         {
+            this.mapper = mapper;
+        }
+        [HttpPost]
+        public IActionResult CreateVehicle([FromBody] VehiclesResources vehicleResources)
+        {
+            var vehicle = mapper.Map<VehiclesResources, Vehicle>(vehicleResources);
             return Ok(vehicle);
         }
     }
--- Mapping/MappingProfile.cs
+++ Mapping/MappingProfile.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using AutoMapper;
 using vega.Controllers.Resources;
 using vega.Models;
@@ -8,9 +9,17 @@ namespace vega.Mapping
     {
         public MappingProfile()
         {
+            //Mapping Domain to API Resources for get API from server
             CreateMap<Make, MakesResources>();
             CreateMap<Model, ModelsResources>();
             CreateMap<Feature, FeatureResources>();
+
+            //Push data from resources to server, we need mapping API to Domain
+            CreateMap<VehiclesResources, Vehicle>()
+                .ForMember(vehicle => vehicle.ContactEmail, opt => opt.MapFrom(vehicleResources => vehicleResources.Contact.Email))
+                .ForMember(vehicle => vehicle.ContactName, opt => opt.MapFrom(vehicleResources => vehicleResources.Contact.Name))
+                .ForMember(vehicle => vehicle.ContactPhone, opt => opt.MapFrom(vehicleResources => vehicleResources.Contact.Phone))
+                .ForMember(vehicle => vehicle.Features, opt => opt.MapFrom(vehicleResources => vehicleResources.Features.Select(id => new VehicleFeature(){FeatureId = id})));
         }
     }
 }
