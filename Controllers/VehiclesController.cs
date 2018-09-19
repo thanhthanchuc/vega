552635c58dd403c8a7b7c0d7c7bed9b3afafc918
toiye <toiye@DESKTOP-BJB9GPG>
2018-09-19 17:02:52 +0700

    Build api for get a vehicle

--- Controllers/VehiclesController.cs
+++ Controllers/VehiclesController.cs
@@ -71,5 +71,18 @@ namespace vega.Controllers
 
             return Ok(id);
         }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetVehicle(int id)
+        {
+            var vehicle = await context.Vehicles.Include(v => v.Features).SingleOrDefaultAsync(v => v.Id == id);
+
+            if (vehicle == null)
+                return NotFound(id);
+
+            var vehicleResources = mapper.Map<Vehicle, VehiclesResources>(vehicle);
+
+            return Ok(vehicleResources);
+        }
     }
 }
