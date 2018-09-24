b38268726c0bfa94bc71d736b0b30f88fc9d7e9a
toiye <toiye@DESKTOP-BJB9GPG>
2018-09-24 16:57:38 +0700

    Submit Form

--- ClientApp/src/services/vehicle.service.ts
+++ ClientApp/src/services/vehicle.service.ts
@@ -13,4 +13,8 @@ export class VehicleService {
   getFeatures() {
     return this.http.get('/api/features').map(res => res.json());
   }
+
+  create(vehicle){
+    return this.http.post('/api/vehicles', vehicle).map(res => res.json());
+  }
 }
