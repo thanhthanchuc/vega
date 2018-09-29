794aa0d79ff97e68f182f3255427e3dcbdbdcc69
toiye <toiye@DESKTOP-BJB9GPG>
2018-09-29 18:54:58 +0700

    Filtering

--- ClientApp/src/app/vehicles-list/vihicles-list.component.ts
+++ ClientApp/src/app/vehicles-list/vihicles-list.component.ts
@@ -1,22 +1,31 @@
+import { KeyValuePair } from './../models/vehicle';
 import { Component, OnInit } from '@angular/core';
 import { Vehicle } from '../models/vehicle';
 import { VehicleService } from '../../services/vehicle.service';
 
 @Component({
-    selector: 'vehicles-list',
+    selector: 'app-vehicles-list',
     templateUrl: 'vehicles-list.component.html',
     styleUrls: ['vehicles-list.component.css']
 })
 
 export class VehiclesListComponent implements OnInit {
-    vehicles : Vehicle[];
+    vehicles: Vehicle[];
+    makes: KeyValuePair[];
+    filter: any = {};
 
     constructor(private vehicleService: VehicleService) {
 
     }
 
     ngOnInit() {
+        this.vehicleService.getMakes().subscribe(makes => this.makes = makes);
+         
         this.vehicleService.getAllVehicles()
             .subscribe(x => this.vehicles = x);
+    }
+
+    onFilterChange() {
+        
     }
 }
