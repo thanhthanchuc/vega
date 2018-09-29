e4aac08574b548b163aabeccba5d550c9bf6ccda
toiye <toiye@DESKTOP-BJB9GPG>
2018-09-29 19:01:44 +0700

    Client-side filtering

--- ClientApp/src/app/vehicles-list/vihicles-list.component.ts
+++ ClientApp/src/app/vehicles-list/vihicles-list.component.ts
@@ -13,6 +13,7 @@ export class VehiclesListComponent implements OnInit {
     vehicles: Vehicle[];
     makes: KeyValuePair[];
     filter: any = {};
+    allVehicles: Vehicle[];
 
     constructor(private vehicleService: VehicleService) {
 
@@ -22,10 +23,22 @@ export class VehiclesListComponent implements OnInit {
         this.vehicleService.getMakes().subscribe(makes => this.makes = makes);
          
         this.vehicleService.getAllVehicles()
-            .subscribe(x => this.vehicles = x);
+            .subscribe(x => this.vehicles = this.allVehicles = x);
     }
 
     onFilterChange() {
+        var vehicles = this.allVehicles;
+
+        if(this.filter.makeId)
+            vehicles = vehicles.filter(v => v.makes.id == this.filter.makeId);
         
+        if(this.filter.modelId)    
+            vehicles = vehicles.filter(v => v.model.id == this.filter.modelId);
+
+        this.vehicles = vehicles;
+    }
+
+    resetFilter() {
+        this.vehicles = this.allVehicles;
     }
 }
